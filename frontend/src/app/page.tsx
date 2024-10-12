600161921d4a5515805b93ef418a1a73b87c4679
pastchum <patrick.navin.thomas123@gmail.com>
2024-10-12 20:55:44 +0800

    Instructions

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -3,7 +3,7 @@ import UploadForm from "@/components/uploadForm";
 
 export default function Home() {
   return (
-    <div className="justify-stretch min-h-screen p-5 pb-20 gap-16 font-[family-name:var(--font-geist-sans)]">
+    <div className="justify-stretch font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-stretch sm:items-start">
         <div className="flex sm:flex-row flex-col justify-stretch justify-items-start p-5 w-full">
           <Instructions />
--- frontend/src/components/instructions.tsx
+++ frontend/src/components/instructions.tsx
@@ -1,7 +1,32 @@
 export default function Instructions() {
   return (
-    <div className="p-3 w-full h-full">
-      <div>test</div>
+    <div className="p-3 w-full h-full border rounded-xl mx-5 shadow relative">
+      <div className="m-5">
+        <div className="border shadow-xl rounded-xl px-5 pt-5 pb-2">
+          <div className="text-teal-700 text-3xl font-bold">
+            Step 1. <div className="text-slate-700">Upload your resume. </div>
+          </div>
+
+          <div className="text-slate-500 m-4 mx-5">
+            Let our ML model analyse your resume.
+          </div>
+        </div>
+        <div className="border shadow-xl rounded-xl px-5 pt-5 pb-2 mt-5">
+          <div className="text-teal-700 text-3xl font-bold">
+            Step 2.<div className=" text-slate-700">Get your results.</div>
+          </div>
+          <div className="text-slate-500 mx-5 m-4">
+            Find out how you can improve your skills and resume to set yourself
+            apart from everyone else!
+          </div>
+        </div>
+        <div className="border shadow-xl rounded-xl px-5 pt-5 pb-2 mt-5">
+          <div className="text-teal-700 text-3xl font-bold">
+            Step 3.<div className=" text-slate-700">Profit.</div>
+          </div>
+          <div className="text-slate-500 mx-5 m-4">Get your dream job!</div>
+        </div>
+      </div>
     </div>
   );
 }
