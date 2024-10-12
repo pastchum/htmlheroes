482e88f4d1a75276b3dd4d6762a74aa53ad1b9e7
pastchum <patrick.navin.thomas123@gmail.com>
2024-10-12 20:10:47 +0800

    changes

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="justify-stretch min-h-screen p-5 pb-20 gap-16 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-stretch sm:items-start">
-        <div className="flex sm:flex-row flex-col justify-between justify-items-start p-5 w-full">
+        <div className="flex sm:flex-row flex-col justify-stretch justify-items-start p-5 w-full">
           <Instructions />
           <Upload />
         </div>
--- frontend/src/components/upload.tsx
+++ frontend/src/components/upload.tsx
@@ -1,10 +1,57 @@
+"use client";
+
+import { useState } from "react";
+
 export default function Upload() {
+  const [file, setFile] = useState(null);
+
+  const handleFileChange = (event) => {
+    setFile(event.target.files[0]);
+  };
+
+  const handleSubmit = async (event) => {
+    if (!file) {
+      alert("Please submit a file for your resume.");
+      return;
+    }
+
+    const formData = new FormData();
+    formData.append("file", file);
+
+    try {
+      const response = await fetch("/api/upload", {
+        method: "POST",
+        body: formData,
+      });
+
+      if (response.ok) {
+        alert("File uploaded successfully!");
+      } else {
+        alert("File upload failed.");
+      }
+    } catch (error) {
+      console.error("Error uploading file:", error);
+      alert("File upload error.");
+    }
+  };
+
   return (
-    <div>
-      <div className="justify-start flex flex-col w-full sm:w-1/2 p-3 rounded-xl border shadow h-full">
-        <div>Upload your file here!</div>
-        <div className="border rounded-xl p-2 justify-center align-center">
-          <div>test</div>
+    <div className="justify-start flex flex-col w-full p-3 rounded-xl border shadow h-full">
+      <div className="font-bold mx-5 my-2">Upload your resume here!</div>
+      <div className="m-2 mx-5 border rounded-xl p-2 justify-center align-center flex">
+        <div className="text-teal-300 text-xs flex-col flex">
+          <form className="flex flex-col">
+            <input
+              className={`${file ? "text-slate-500" : "text-red-500"}`}
+              type="file"
+              onChange={handleFileChange}
+            />
+            <button type="submit">
+              <div className="border m-5 p-1 rounded-xl bg-teal-400 text-slate-50">
+                Upload
+              </div>
+            </button>
+          </form>
         </div>
       </div>
     </div>
